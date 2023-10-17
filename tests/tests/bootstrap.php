1343bbf1b9ef626f336daf5ddc0ab3e754dff955
Damijan Cavar <damijan.cavar@turbinekreuzberg.com>
2023-10-17 11:02:06 +0200

    adding codeception boostrap to be able to use Spryker helpers

---
+++ tests/tests/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+use Spryker\Shared\Config\Config;
+
+if (!defined('APPLICATION_ROOT_DIR')) {
+    define('APPLICATION_ROOT_DIR', __DIR__ .'/../App/');
+}
+if (!defined('APPLICATION_STORE')) {
+    define('APPLICATION_STORE', 'DE');
+}
+if (!defined('APPLICATION_ENV')) {
+    define('APPLICATION_ENV', 'dev');
+}
+
+$config = Config::getInstance();
+$config->init();
